a1d67f77c2ffbe60d05bf358f436c9098f4f9f95
Paulo1402 <paulobenatto02@gmail.com>
2023-09-19 18:27:44 -0300

    work on content page

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-
+  @Input() photoCover: string = ''
+  @Input() contentTitle: string = ''
+  @Input() contentDescription: string = ''
 }
