f115f8895019ea2af850a8765a572b0df2018254
Paulo1402 <paulobenatto02@gmail.com>
2023-09-20 11:12:30 -0300

    add data to dataFake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,34 @@
 export const dataFake = [
   {
-    'id': 1,
-    'photo': 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
-    'title': 'BIG CARD',
-    'description': 'Lorem Ipsum',
+    id: 1,
+    photoCover:
+      'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/01/07093212/PYTHON.png',
+    cardTitle: 'Python 4.0: As Novas Funcionalidades que Vão Transformar a Programação',
+    cardDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus non erat congue tempor sed blandit quam. Phasellus posuere ullamcorper orci, at feugiat turpis rhoncus in. Curabitur tristique dui vel varius ultricies. Nam vitae placerat felis, at dictum lacus. Nunc ornare id tortor eu vehicula. Aliquam sit amet dui dui. In massa nunc, suscipit nec pellentesque sed, tempor at eros. Donec ac mi et dolor pellentesque vulputate. Nulla eros lacus, ultrices sed ultricies et, facilisis id nisl. Nulla pulvinar ut tellus non laoreet. Vestibulum eget convallis est. Suspendisse potenti. Vestibulum sed venenatis sapien.\nNullam euismod dignissim massa et eleifend. Donec id nisi hendrerit, suscipit dolor et, efficitur quam. Nulla quis sapien ornare, blandit libero eu, ultrices nisi. Nam a urna quis leo scelerisque efficitur. Vivamus ultricies massa sodales ligula ultricies viverra. Integer sagittis magna cursus porttitor hendrerit. Sed pretium massa at arcu euismod, eget aliquam risus ornare. Aliquam non dui sit amet arcu sollicitudin finibus sed et lectus. Donec nec auctor tortor, sit amet congue lorem. Ut tellus enim, auctor cursus imperdiet in, pharetra sed sem. Sed sollicitudin nibh non tortor pretium faucibus. Proin aliquam odio vitae feugiat aliquet. Sed sagittis quam magna.',
   },
   {
-    'id': 2,
-    'photo': 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
-    'title': 'SMALL CARD',
-    'description': 'Lorem Ipsum',
+    id: 2,
+    photoCover:
+      'https://www.resilia.com.br/wp-content/uploads/2022/07/1_6ahbWjp_g9hqhaTDSJOL1Q-1200x675.png',
+    cardTitle: 'JavaScript Quantum: A Próxima Fronteira na Programação Web',
+    cardDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus non erat congue tempor sed blandit quam. Phasellus posuere ullamcorper orci, at feugiat turpis rhoncus in. Curabitur tristique dui vel varius ultricies. Nam vitae placerat felis, at dictum lacus. Nunc ornare id tortor eu vehicula. Aliquam sit amet dui dui. In massa nunc, suscipit nec pellentesque sed, tempor at eros. Donec ac mi et dolor pellentesque vulputate. Nulla eros lacus, ultrices sed ultricies et, facilisis id nisl. Nulla pulvinar ut tellus non laoreet. Vestibulum eget convallis est. Suspendisse potenti. Vestibulum sed venenatis sapien.\nNullam euismod dignissim massa et eleifend. Donec id nisi hendrerit, suscipit dolor et, efficitur quam. Nulla quis sapien ornare, blandit libero eu, ultrices nisi. Nam a urna quis leo scelerisque efficitur. Vivamus ultricies massa sodales ligula ultricies viverra. Integer sagittis magna cursus porttitor hendrerit. Sed pretium massa at arcu euismod, eget aliquam risus ornare. Aliquam non dui sit amet arcu sollicitudin finibus sed et lectus. Donec nec auctor tortor, sit amet congue lorem. Ut tellus enim, auctor cursus imperdiet in, pharetra sed sem. Sed sollicitudin nibh non tortor pretium faucibus. Proin aliquam odio vitae feugiat aliquet. Sed sagittis quam magna.',
   },
-]
+  {
+    id: 3,
+    photoCover:
+      'https://miro.medium.com/v2/resize:fit:783/1*Kj8Eq401fP2ecTY8r9B89Q.png',
+    cardTitle: 'Angular 12 Lançado: Descubra as Novas Ferramentas e Melhorias',
+    cardDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus non erat congue tempor sed blandit quam. Phasellus posuere ullamcorper orci, at feugiat turpis rhoncus in. Curabitur tristique dui vel varius ultricies. Nam vitae placerat felis, at dictum lacus. Nunc ornare id tortor eu vehicula. Aliquam sit amet dui dui. In massa nunc, suscipit nec pellentesque sed, tempor at eros. Donec ac mi et dolor pellentesque vulputate. Nulla eros lacus, ultrices sed ultricies et, facilisis id nisl. Nulla pulvinar ut tellus non laoreet. Vestibulum eget convallis est. Suspendisse potenti. Vestibulum sed venenatis sapien.\nNullam euismod dignissim massa et eleifend. Donec id nisi hendrerit, suscipit dolor et, efficitur quam. Nulla quis sapien ornare, blandit libero eu, ultrices nisi. Nam a urna quis leo scelerisque efficitur. Vivamus ultricies massa sodales ligula ultricies viverra. Integer sagittis magna cursus porttitor hendrerit. Sed pretium massa at arcu euismod, eget aliquam risus ornare. Aliquam non dui sit amet arcu sollicitudin finibus sed et lectus. Donec nec auctor tortor, sit amet congue lorem. Ut tellus enim, auctor cursus imperdiet in, pharetra sed sem. Sed sollicitudin nibh non tortor pretium faucibus. Proin aliquam odio vitae feugiat aliquet. Sed sagittis quam magna.',
+  },
+  {
+    id: 4,
+    photoCover:
+      'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124',
+    cardTitle: 'React 18 Revelado: Novos Recursos Revolucionários para Desenvolvedores',
+    cardDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus non erat congue tempor sed blandit quam. Phasellus posuere ullamcorper orci, at feugiat turpis rhoncus in. Curabitur tristique dui vel varius ultricies. Nam vitae placerat felis, at dictum lacus. Nunc ornare id tortor eu vehicula. Aliquam sit amet dui dui. In massa nunc, suscipit nec pellentesque sed, tempor at eros. Donec ac mi et dolor pellentesque vulputate. Nulla eros lacus, ultrices sed ultricies et, facilisis id nisl. Nulla pulvinar ut tellus non laoreet. Vestibulum eget convallis est. Suspendisse potenti. Vestibulum sed venenatis sapien.\nNullam euismod dignissim massa et eleifend. Donec id nisi hendrerit, suscipit dolor et, efficitur quam. Nulla quis sapien ornare, blandit libero eu, ultrices nisi. Nam a urna quis leo scelerisque efficitur. Vivamus ultricies massa sodales ligula ultricies viverra. Integer sagittis magna cursus porttitor hendrerit. Sed pretium massa at arcu euismod, eget aliquam risus ornare. Aliquam non dui sit amet arcu sollicitudin finibus sed et lectus. Donec nec auctor tortor, sit amet congue lorem. Ut tellus enim, auctor cursus imperdiet in, pharetra sed sem. Sed sollicitudin nibh non tortor pretium faucibus. Proin aliquam odio vitae feugiat aliquet. Sed sagittis quam magna.',
+  },
+];
